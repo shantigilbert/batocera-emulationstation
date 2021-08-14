d9881af0bb8b5ddee40f69ef5373590a7295b6a7
joslan <Langerz82>
2021-08-14 12:51:07 +1000

    Fix to show resolution change when no emulator or core options.

--- es-app/src/guis/GuiGamelistOptions.cpp
+++ es-app/src/guis/GuiGamelistOptions.cpp
@@ -298,7 +298,8 @@ GuiGamelistOptions::GuiGamelistOptions(Window* window, IGameListView* gamelist, 
 			auto srcSystem = file->getSourceFileData()->getSystem();
 			auto sysOptions = mSystem->isGroupSystem() ? srcSystem : mSystem;
 
-			bool showSystemOptions = ApiSystem::getInstance()->isScriptingSupported(ApiSystem::GAMESETTINGS) && (sysOptions->hasFeatures() || sysOptions->hasEmulatorSelection());
+			//bool showSystemOptions = ApiSystem::getInstance()->isScriptingSupported(ApiSystem::GAMESETTINGS) && (sysOptions->hasFeatures() || sysOptions->hasEmulatorSelection());
+			bool showSystemOptions = true;
 			/*
 			bool showGameOptions = (file != nullptr && file->getType() != FOLDER);
 
