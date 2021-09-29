58787b93994d734d5165a7ce2971962d98224925
Langerz82 <langley.joshua@gmail.com>
2021-09-29 17:36:18 +0900

    syntax fix.

--- es-app/src/FileSorts.cpp
+++ es-app/src/FileSorts.cpp
@@ -104,8 +104,8 @@ namespace FileSorts
 		std::string name1 = file1->getSortNameRaw();
 		std::string name2 = file2->getSortNameRaw();
 		if (name1.empty() && name2.empty()) {
-			name1 = file1->getName();
-			name2 = file2->getName();
+			name1 = ((FileData*)file1)->getName();
+			name2 = ((FileData*)file2)->getName();
 		}
 		if (name1.empty()) return false;
 		if (name2.empty()) return true;
