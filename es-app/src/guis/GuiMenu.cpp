675ad193dfe4245f482b25d72b4f9cac66a66e37
Langerz82 <langley.joshua@gmail.com>
2022-01-19 18:57:29 +1100

    add create joy remapping 27

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -4237,22 +4237,22 @@ void GuiMenu::deleteBtnJoyCfg(Window *mWindow, GuiSettings *systemConfiguration,
 	systemConfiguration->addWithLabel(_("DELETE REMAP"), deleteOption);	
 	
 	systemConfiguration->addSaveFunc([mWindow, deleteOption, prefixName, arr_joy_btn_names] {
-		int index = atoi(deleteOption.getSelected().c_str());
+		int index = atoi(deleteOption->getSelected().c_str());
 		if (index == -1)
 			return;
 
 		index++;
-		arr_joy_btn_names.erase(arr_joy_btn_names.begin() + index);
+		arr_joy_btn_names->erase(arr_joy_btn_names.begin() + index);
 		
-		remapNames = "";
+		std::string remapNames = "";
 		for(int i=0; i < arr_joy_btn_names.size(); ++i)
 		{
 			if (i > 0)
 				remapNames += ",";
-			remapNames += arr_joy_btn_names[i]->getSelected();
+			remapNames += arr_joy_btn_names[i];
 		}		
 		SystemConf::getInstance()->set(prefixName + ".joy_btn_names", remapNames);
-		SystemConf::getInstance()->set(prefixName + ".joy_btn_order", index);
+		SystemConf::getInstance()->set(prefixName + ".joy_btn_order"+index, "");
 		SystemConf::getInstance()->saveSystemConf();
 	});
 }
