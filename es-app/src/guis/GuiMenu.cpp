04157fd7cef840c85288a9f90d352b955e22d0ca
Langerz82 <langley.joshua@gmail.com>
2022-01-19 22:49:20 +1100

    test3

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -4365,18 +4365,18 @@ void GuiMenu::popSpecificConfigurationGui(Window* mWindow, std::string title, st
 		tEmulator = systemData->getEmulator(true);
 	if (!tEmulator.empty() && systemData->isFeatureSupported(tEmulator, currentCore, EmulatorFeatures::joybtnremap))
 	{
-		[&, mWindow, systemConfiguration, configName, tEmulator] {
-			if (SystemConf::getInstance()->get(configName + ".joy_btns").empty() ||
-					SystemConf::getInstance()->get(configName + ".joy_btn_count").empty() ||
-					SystemConf::getInstance()->get(configName + ".joy_btn_map_count").empty() ||
-					SystemConf::getInstance()->get(configName + ".joy_btn_names").empty())
+		[&] {
+			if (SystemConf::getInstance()->get(tEmulator + ".joy_btns").empty() ||
+					SystemConf::getInstance()->get(tEmulator + ".joy_btn_count").empty() ||
+					SystemConf::getInstance()->get(tEmulator + ".joy_btn_map_count").empty() ||
+					SystemConf::getInstance()->get(tEmulator + ".joy_btn_names").empty())
 			{
 				mWindow->pushGui(new GuiMsgBox(mWindow, 
-					configName + " /n" +
-					SystemConf::getInstance()->get(configName + ".joy_btns") + " \n" +
-					SystemConf::getInstance()->get(configName + ".joy_btn_count") + " \n" +
-					SystemConf::getInstance()->get(configName + ".joy_btn_map_count") + " \n" +
-					SystemConf::getInstance()->get(configName + ".joy_btn_names") + " \n",
+					tEmulator + " /n" +
+					SystemConf::getInstance()->get(tEmulator + ".joy_btns") + " \n" +
+					SystemConf::getInstance()->get(tEmulator + ".joy_btn_count") + " \n" +
+					SystemConf::getInstance()->get(tEmulator + ".joy_btn_map_count") + " \n" +
+					SystemConf::getInstance()->get(tEmulator + ".joy_btn_names") + " \n",
 					_("OK"), nullptr));
 				return;
 			}
