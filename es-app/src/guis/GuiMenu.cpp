3532acb1e012ed178e00c0aba821e23bbf13f08e
Langerz82 <langley.joshua@gmail.com>
2022-01-20 01:05:29 +1100

    t14

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -4246,7 +4246,7 @@ void GuiMenu::deleteBtnJoyCfg(Window *mWindow, GuiSettings *systemConfiguration,
 			return;
 		
 		mWindow->pushGui(new GuiMsgBox(mWindow, _("ARE YOU SURE YOU WANT TO DELETE THE REMAP?"),
-			_("YES"), [&, mWindow, deleteOption, prefixName, arr_joy_btn_names]
+			_("YES"), [&, mWindow, deleteOption, prefixName, &arr_joy_btn_names]
 			{
 				//std::vector<std::string> l_arr_joy_btn_names(arr_joy_btn_names);
 				int index = atoi(deleteOption->getSelected().c_str());
