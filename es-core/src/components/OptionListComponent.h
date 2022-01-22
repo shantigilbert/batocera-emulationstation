51b8d14b4a1f8e3391c67fcf9f939e385e55ed46
Langerz82 <langley.joshua@gmail.com>
2022-01-22 19:15:37 +1100

    c39

--- es-core/src/components/OptionListComponent.h
+++ es-core/src/components/OptionListComponent.h
@@ -24,6 +24,10 @@
 #define CHECKED_PATH ":/checkbox_checked.svg"
 #define UNCHECKED_PATH ":/checkbox_unchecked.svg"
 
+#ifdef _ENABLEEMUELEC
+class OptionListPopup;
+#endif
+
 template<typename T>
 class OptionListComponent : public GuiComponent
 {
