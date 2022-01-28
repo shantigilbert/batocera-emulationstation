74908bb807a842294af7fe7b1fdd2445d7468069
Langerz82 <langley.joshua@gmail.com>
2022-01-28 17:00:18 +1100

    t60

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -377,14 +377,14 @@ void GuiMenu::openEmuELECSettings()
 		for (auto it = Audiodevices.cbegin(); it != Audiodevices.cend(); it++)
 		emuelec_audiodev_def->add(*it, *it, AudiodevicesS == *it);
 		
-		s->addWithLabel(_("AUDIO DEVICE"), emuelec_audiodev_def);
-		s->addSaveFunc([emuelec_audiodev_def] {
-			if (emuelec_audiodev_def->changed()) {
-				std::string selectedaudiodev = emuelec_audiodev_def->getSelected();
-				SystemConf::getInstance()->set("ee_audio_device", selectedaudiodev);
-				SystemConf::getInstance()->saveSystemConf();
-			}
-		});
+		s->addWithDescription(_("AUDIO DEVICE"), _("Changes will need an EmulationStation restart."), emuelec_audiodev_def);
+        
+        emuelec_audiodev_def->setSelectedChangedCallback([emuelec_audiodev_def](std::string name) { 
+            if (SystemConf::getInstance()->set("ee_audio_device", name)) 
+                SystemConf::getInstance()->saveSystemConf();
+                std::string selectedaudio = emuelec_audiodev_def->getSelected();
+                runSystemCommand("/usr/bin/emuelec-utils setauddev " +selectedaudio, "", nullptr);
+            });
 #endif
         auto bluetoothd_enabled = std::make_shared<SwitchComponent>(mWindow);
 		bool btbaseEnabled = SystemConf::getInstance()->get("ee_bluetooth.enabled") == "1";
@@ -2501,11 +2501,13 @@ void GuiMenu::openGamesSettings_batocera()
 				decorations_window->addInputTextRow(_("CUSTOM .PNG IMAGE PATH"), "global.bezel.tattoo_file", false);
 
 				auto bezel_resize_tattoo = std::make_shared<SwitchComponent>(mWindow);
-				bezel_resize_tattoo->setState(!SystemConf::getInstance()->getBool("global.bezel.resize_tattoo"));
+				bezel_resize_tattoo->setState(SystemConf::getInstance()->getBool("global.bezel.resize_tattoo"));
 				decorations_window->addWithDescription(_("RESIZE TATTOO"), _("Shrink/expand tattoo to fit within the bezel's border."), bezel_resize_tattoo);
 				decorations_window->addSaveFunc([bezel_resize_tattoo]
 				{
-					SystemConf::getInstance()->setBool("global.bezel.resize_tattoo", bezel_resize_tattoo->getState());
+					if (SystemConf::getInstance()->getBool("global.bezel.resize_tattoo") != bezel_resize_tattoo->getState()) {
+						SystemConf::getInstance()->setBool("global.bezel.resize_tattoo", bezel_resize_tattoo->getState());
+					}
 				});
 
 				mWindow->pushGui(decorations_window);
