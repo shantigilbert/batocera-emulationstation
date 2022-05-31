954f94aa7417216254d46856ce37ec1828db6c3b
Langerz82 <langley.joshua@gmail.com>
2022-05-31 18:13:08 +1000

    f6

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -302,7 +302,7 @@ void GuiMenu::refreshView(Window* window, bool deleteCurrentGui)
 		GuiComponent* topGui = window->peekGui();
 		if (topGui != nullptr)
 			window->removeGui(topGui);
-	}
+	}*/
 
 	GuiComponent *gui;
 	while ((gui = window->peekGui()) != NULL)
@@ -311,14 +311,14 @@ void GuiMenu::refreshView(Window* window, bool deleteCurrentGui)
 
 		//if (gui != sInstance)
 		delete gui;
-	}*/
+	}
 
 	ViewController::deinit();
 
 	ViewController::init(window);
 	
 	//CollectionSystemManager::init(window);		
-	//SystemData::loadConfig(window);
+	SystemData::loadConfig(window);
 	
 	//ViewController::get()->goToSystemView(systemName, true, viewMode);	
 	//ViewController::get()->reloadAll(nullptr, false); // Avoid reloading themes a second time
