d6c50ace8e9c51304ec29c57d31c912c03edb400
Langerz82 <langley.joshua@gmail.com>
2023-01-01 15:24:48 +1100

    f14

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -47,7 +47,7 @@
 
 #ifdef _ENABLEEMUELEC	
 	#include <atomic>
-	//#include <thread>
+	#include <thread>
 #endif
 
 static std::string gPlayVideo;
@@ -656,7 +656,7 @@ int main(int argc, char* argv[])
 	int lastTime = SDL_GetTicks();
 	int ps_time = SDL_GetTicks();
 #ifdef _ENABLEEMUELEC
-	std::atomic<int> bt_pid = 0;
+	std::atomic<int> bt_pid(0);
 #endif
 	bool running = true;
 
