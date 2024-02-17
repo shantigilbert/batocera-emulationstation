ab4f5166a928fdc81c73c9c461b50d7dabf53673
Joshua L <Langerz82@users.noreply.github.com>
2024-02-17 12:56:43 +1100

    rm direct restart calls.

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -365,7 +365,6 @@ void GuiMenu::openEmuELECSettings()
 					LOG(LogInfo) << "Setting video to " << selectedVideoMode;
 					//Utils::Platform::ProcessStartInfo("/usr/bin/setres.sh").run();
 					SystemConf::getInstance()->saveSystemConf();
-					Utils::Platform::runRestartESCommand();
 					Scripting::fireEvent("quit", "restart");
 					Utils::Platform::quitES(Utils::Platform::QuitMode::QUIT);
 				//	v_need_reboot = true;
@@ -883,7 +882,6 @@ void GuiMenu::addFrameBufferOptions(Window* mWindow, GuiSettings* guiSettings, s
 		SystemConf::getInstance()->set(cfgName, selectedFB);
 
 		if (configName == "ee_es.") {
-			Utils::Platform::runRestartESCommand();
 			Scripting::fireEvent("quit", "restart");
 			Utils::Platform::quitES(Utils::Platform::QuitMode::QUIT);
 		}
@@ -4578,7 +4576,6 @@ void GuiMenu::openQuitMenu_static(Window *window, bool quickAccessMenu, bool ani
 	s->addEntry(_("RESTART EMULATIONSTATION"), false, [window] {
 		window->pushGui(new GuiMsgBox(window, _("REALLY RESTART EMULATIONSTATION?"), _("YES"),
 			[] {
-    		   Utils::Platform::runRestartESCommand();
     		   Scripting::fireEvent("quit", "restart");
 			   Utils::Platform::quitES(Utils::Platform::QuitMode::QUIT);
 		}, _("NO"), nullptr));
