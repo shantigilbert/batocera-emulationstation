8c07e0d1c60b31cd1b58a94d786fac56f26abf26
Joshua L <Langerz82@users.noreply.github.com>
2024-08-18 15:55:05 +1000

    Update GuiMenu.cpp

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -5843,7 +5843,6 @@ void GuiMenu::popSpecificConfigurationGui(Window* mWindow, std::string title, st
 
 		std::vector<std::string> midi_output;
 		std::string def_midi;
-		midi_output.push_back("none");
 		std::string midi_cmd = "/usr/bin/emuelec-utils midi_output "+currentEmulator+" "+currentCore;
 		for(std::stringstream ss(Utils::Platform::getShOutput(midi_cmd.c_str())); getline(ss, def_midi, ','); ) {
 			if (!std::count(midi_output.begin(), midi_output.end(), def_midi)) {
@@ -5854,14 +5853,14 @@ void GuiMenu::popSpecificConfigurationGui(Window* mWindow, std::string title, st
 		//midi_output.push_back("mt32d");
 		//midi_output.push_back("fluidsynth");
 		std::string saved_midi = SystemConf::getInstance()->get(configName+".ra_midi_output");
-		ra_midi_def->add("auto", "auto", saved_midi.empty());
+		ra_midi_def->add("none", "none", saved_midi.empty());
 		for (auto it = midi_output.cbegin(); it != midi_output.cend(); it++)
 			ra_midi_def->add(*it, *it, saved_midi == *it);
 		systemConfiguration->addWithLabel(_("RETROARCH MIDI"), ra_midi_def);
 		systemConfiguration->addSaveFunc([ra_midi_def, configName] {
 			if (ra_midi_def->changed()) {
 				std::string selectedMidiOutput = ra_midi_def->getSelected();
-				if (selectedMidiOutput == "auto")
+				if (selectedMidiOutput == "none")
 					selectedMidiOutput="";
 				SystemConf::getInstance()->set(configName+".ra_midi_output", selectedMidiOutput);
 				SystemConf::getInstance()->saveSystemConf();
