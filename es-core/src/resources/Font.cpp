161f30e5c8aef1540f780340bd95630a751845ce
Joshua L <Langerz82@users.noreply.github.com>
2024-08-29 22:12:09 +1000

    t10

--- es-core/src/resources/Font.cpp
+++ es-core/src/resources/Font.cpp
@@ -207,7 +207,7 @@ bool Font::FontTexture::findEmpty(const Vector2i& size, Vector2i& cursor_out)
 	}
 
 	cursor_out = writePos;
-	writePos[0] += size.x() + 4; // leave 1px of space between glyphs
+	writePos[0] += size.x() + 2; // leave 1px of space between glyphs
 
 	if(size.y() > rowHeight)
 		rowHeight = size.y();
